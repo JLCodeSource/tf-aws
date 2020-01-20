6916c9cdab5ed4b9d6818ff424f6844e531d7009
JLCodeSource <JLCodeSource@gmail.com>
2020-01-20 16:59:15 +0000

    added prv; changed rst to trt

--- site/pub_priv_vpc.tf
+++ site/pub_priv_vpc.tf
@@ -9,22 +9,22 @@
 # BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under the License.
 
-resource "aws_vpc" "rst" {
+resource "aws_vpc" "trt" {
   cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   tags = {
-      Name = "rst_vpc"
+      Name = "trt_vpc"
   }
 }
 
-resource "aws_internet_gateway" "rst" {
-  vpc_id = aws_vpc.rst.id
+resource "aws_internet_gateway" "trt" {
+  vpc_id = aws_vpc.trt.id
   tags = {
-      Name = "rst_igw"
+      Name = "trt_igw"
   }
 }
 output "vpc_id" {
-  value = aws_vpc.rst.id
+  value = aws_vpc.trt.id
 }
 
 ### NAT Instance todo
@@ -33,64 +33,75 @@ output "vpc_id" {
 # Subnets
 #
 
-resource "aws_subnet" "rst_pub_subnets" {
-  vpc_id = aws_vpc.rst.id
+resource "aws_subnet" "trt_pub_subnets" {
+  vpc_id = aws_vpc.trt.id
   count = length(var.subnets)
 
   cidr_block = cidrsubnet(var.vpc_cidr, var.newbits, count.index)
   availability_zone = var.subnets[count.index]
   tags = {
-    Name = "rst_public_subnet_${var.subnets[count.index]}"
+    Name = "trt_public_subnet_${var.subnets[count.index]}"
   }
 }
 
-resource "aws_subnet" "rst_prv_subnets" {
-  vpc_id = aws_vpc.rst.id
+resource "aws_subnet" "trt_prv_subnets" {
+  vpc_id = aws_vpc.trt.id
   count = length(var.subnets)
 
   cidr_block = cidrsubnet(var.vpc_cidr, var.newbits, count.index+4)
   tags = {
-    Name = "rst_private_subnet_${var.subnets[count.index]}"
+    Name = "trt_private_subnet_${var.subnets[count.index]}"
     availability_zone = var.subnets[count.index]
   }
 }
 
-/* data "aws_subnet" "rst_pub_subnets" {
-  
-  count = length(var.subnets)
-  
-  filter {
-    Name = "rst_public_subnet_${var.subnets[count.index]}"
-  }
-
-  id[count.index] = data.aws_subnet.rst_pub_subnets.id[count.index]
-
-} */
-
-resource "aws_route_table" "rst_pub_rt" {
-  vpc_id = aws_vpc.rst.id
+resource "aws_route_table" "trt_pub_rt" {
+  vpc_id = aws_vpc.trt.id
 
   count = length(var.subnets)
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.rst.id
+    gateway_id = aws_internet_gateway.trt.id
   }
   tags = {
-    Name = "rst_public_route_table_${var.subnets[count.index]}"
+    Name = "trt_public_route_table_${var.subnets[count.index]}"
   }
 }
 
-locals {
-  rst_pub_subnets_ids = aws_subnet.rst_pub_subnets.*.id
-  rst_pub_rt_ids = aws_route_table.rst_pub_rt.*.id
-  rst_priv_subnets_ids = aws_subnet.rst_prv_subnets.*.id
+resource "aws_route_table" "trt_prv_rt" {
+  vpc_id = aws_vpc.trt.id
+
+  count = length(var.subnets)
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.trt.id
+  }
+  tags = {
+    Name = "trt_prv_route_table_${var.subnets[count.index]}"
+  }
 }
 
 
-resource "aws_route_table_association" "rst_pub_subnets" {
-  count = length(local.rst_pub_rt_ids)
+locals {
+  trt_pub_subnets_ids = aws_subnet.trt_pub_subnets.*.id
+  trt_pub_rt_ids = aws_route_table.trt_pub_rt.*.id
+  trt_prv_subnets_ids = aws_subnet.trt_prv_subnets.*.id
+  trt_prv_rt_ids = aws_route_table.trt_prv_rt.*.id
+}
 
-  subnet_id = local.rst_pub_subnets_ids[count.index]
-  route_table_id = local.rst_pub_rt_ids[count.index]
+
+resource "aws_route_table_association" "trt_pub_subnets" {
+  count = length(local.trt_pub_rt_ids)
+
+  subnet_id = local.trt_pub_subnets_ids[count.index]
+  route_table_id = local.trt_pub_rt_ids[count.index]
+}
+
+resource "aws_route_table_association" "trt_prv_subnets" {
+  count = length(local.trt_prv_rt_ids)
+
+  subnet_id = local.trt_prv_subnets_ids[count.index]
+  route_table_id = local.trt_prv_rt_ids[count.index]
 }
